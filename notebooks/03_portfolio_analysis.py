0f29e957914b02f4fcedde6aaa1d24b23588d5ed
ADITYA KUMAR <adtkmr.contact@gmail.com>
2026-01-25 22:24:00 +0530

    Update 03_portfolio_analysis.py

--- notebooks/03_portfolio_analysis.py
+++ notebooks/03_portfolio_analysis.py
@@ -3,3 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 returns = pd.read_csv('../data/processed/stock_returns.csv', index_col=0, parse_dates=True)
+
+
+
+n_assets = returns.shape[1]
+weights = np.array([1 / n_assets] * n_assets)
+
